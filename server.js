814fd24c378c52ccc60de5c1b193988eaf1fb3a3
Hyunsu Joo <hs9880@gmail.com>
2020-10-11 13:59:10 +0900

    add initial server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express =require('express');
+const app = express();
+app.use(express.static(__dirname+'/public'));
+const socketio = require('socket.io');
+const expressServer= app.listen(8080,()=>{
+    console.log("Express and socketio are listening on port 8080");
+});
+
+const io = socketio(expressServer);
+
+//App organization 
+
+module.exports={
+    app,
+    io
+}

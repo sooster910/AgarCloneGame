f743d30ecdfdcee1d9368a76dad1a727718a1142
Hyunsu Joo <hs9880@gmail.com>
2020-10-14 23:42:56 +0900

    Add logic for connecting socket with orb

--- public/socket.js
+++ public/socket.js
@@ -1,3 +1,11 @@
+//create connection to server 
+//io is coming from socket.io cdn
 let socket = io.connect('http://localhost:8080');
 
 console.log('socket',socket)
+
+
+//listen for init (where is emitting init ? socketMain.js from server)
+socket.on('init',(data)=>{
+    console.log('data',data);
+})
--- sockets/socketMain.js
+++ sockets/socketMain.js
@@ -1,18 +1,22 @@
-const io = require('../server').io;
-const Orb = require('./classes/Orb');
-let orbs  =[];
+const io = require('../servers').io
+console.log('ioioio',io)
 
-initGame();
-io.sockets.on('connect',(socket)=>{
-    socket.emit('init',{orbs})
+const Orb = require('./classes/Orb')
+let orbs = [];
+
+initGame()
+
+io.sockets.on('connect', (socket) => {
+    socket.emit('init', {
+        orbs
+    })
 })
 
-//Run at the beginning of a new game
-
-function initGame(){
-    for(let i=0; i <500; i++){
-        orbs.push(new Orb());
+// Run at the beginning of a new game
+function initGame() {
+    for(let i = 0; i < 500; i++) {
+        orbs.push(new Orb())
     }
 }
-module.exports = io;
- 
+
+module.exports = io

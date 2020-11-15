491fcaf3d07056fe9884f36e481bfdfd748e94a9
Hyunsu Joo <hs9880@gmail.com>
2020-11-15 21:36:01 +0900

    make setting as a module

---
+++ sockets/defaultSettings.js
@@ -0,0 +1,12 @@
+module.exports={
+  
+        defaultObs : 500,
+        defaultSpeed : 6,
+        defaultSize: 10,
+        //as player gets bigger, the zoom needs to go out
+        //becuase once player gets really big, it's very possible for the orb to take up the whole screen. 
+        defaultZoom: 1.5,
+        worldWidth:500,
+        worldHeight:500
+    
+}
